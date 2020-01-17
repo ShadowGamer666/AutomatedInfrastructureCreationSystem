c02a6e3647ec022c24307faea02dfc397a15df76
ShadowGamer666 <32739605+ShadowGamer666@users.noreply.github.com>
2020-01-17 14:55:24 +0000

    Compatibility changes to parameters.

--- Terraform/Projects/aws_single_nodb_server/main.tf
+++ Terraform/Projects/aws_single_nodb_server/main.tf
@@ -7,7 +7,7 @@ variable "region" {}
 variable "project_name" {}
 variable "db_username" {}
 variable "db_password" {}
-variable "public_key_filepath" {}
+variable "rds_subnet_group_name" {}
 variable "ec2_type" {
   default = "t3.micro"
 }
@@ -29,7 +29,6 @@ variable "project_tags" {
 module "aws_single_ec2_instance" {
   source = "../../Modules/aws_ec2/standard_ec2"
   ec2_project_name = "${var.project_name}"
-  ec2_public_key = "${var.public_key_filepath}"
   ec2_type = "${var.ec2_type}"
   ec2_default_ami = "${var.ec2_use_default_ami}"
   ec2_tags = "${var.project_tags}"
