6d286ca5f6fa1623b7d9abd7ea7ffd5c40b770c4
ShadowGamer666 <32739605+ShadowGamer666@users.noreply.github.com>
2020-02-17 18:59:28 +0000

    Added Ansible AutoML Extraction Model Integration.

--- InfraBash.sh
+++ InfraBash.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
-echo "Script Found!!!"
+echo "----------"
+echo "Script Found !!!!!"
+echo "----------"
+
 FILETYPE=$1
 # Reads in user credentials for the chosen cloud provider.
 CREDENTIALS=`openssl rsautl -decrypt -inkey /opt/infra/infra_exchange_key.pem -in /tmp/encryptcreds.txt`
@@ -15,7 +18,7 @@ export GOOGLE_APPLICATION_CREDENTIALS="/opt/infra/automl.json"
 echo "----------"
 echo "Peformining Classification Analysis on SRS data ....."
 echo "----------"
-CLASS_RESULT=`python3 /opt/infra/InfraPython.py $FILETYPE`
+CLASS_RESULT=`python3 /opt/infra/InfraPythonClass.py $FILETYPE`
 # Ensures that Model Prediction was successful.
 PREDICTION_SUCCESS=$?
 if [ $PREDICTION_SUCCESS -ne 0 ]
@@ -36,6 +39,13 @@ echo "Beginning Infrastructure Creation for "$PROJECT_NAME" ....."
 echo "----------"
 # Splits the user credentials into their individual elements.
 TEMPLATE_NAME=`echo $CLASS_RESULT | cut -d' ' -f1`
+# Records for Ansible if the template requires server configuration.
+if [ $TEMPLATE_NAME = "mobile_bucket_noserver" ] || [ $TEMPLATE_NAME = "mobile_dbcluster_noserver" ] || [ $TEMPLATE_NAME = "mobile_db_noserver" ]
+then
+        SERVER=false
+else
+        SERVER=true
+fi
 CLOUD_PROVIDER=`echo $CREDENTIALS | cut -d' ' -f1`
 # Gets the Infra Application Root Directory from Env Variable.
 INFRA_PATH="/opt/infra"
@@ -73,10 +83,10 @@ then
         cd $INFRA_PATH/InfrastructureLib/Terraform/Projects/aws_$TEMPLATE_NAME
 elif [ $CLOUD_PROVIDER = "GOOGLE" ]
 then
-        echo "Google Cloud support will come in the future."
+        echo "Google Cloud Support will come in the future."
 elif [ $CLOUD_PROVIDER = "AZURE" ]
 then
-        echo "Azure support will come in the future."
+        echo "Azure Support will come in the future."
 fi
 echo "----------"
 echo "Beginning Terraform Creation Process ....."
@@ -113,12 +123,47 @@ else
         echo "----------"
 fi
 
-# Executes any required Ansible Playbooks to configure the new infrastructure.
+# Runs the Google AutoML Extraction Model script.
+echo "----------"
+echo "Performing Extraction Analysis on SRS Data ....."
+echo "----------"
+# Resets to Root Directory to get prediction from Extraction model.
+cd $INFRA_PATH
+# Discovers if any configuration is considered nessecary by the Extraction Model.
+EXTRACT_RESULT=`python3 InfraPythonExtract.py $FILETYPE`
+# Ensures that Model Prediction was successful.
+EXTRACTION_SUCCESS=$?
+if [ $EXTRACTION_SUCCESS -ne 0 ]
+then
+        echo "xxxxxxxxxx"
+        echo "Model Prediction Failure Detected !!!!!"
+        echo "xxxxxxxxxx"
+        exit 16
+fi
+# Sets Server flag to False if Extraction Model suggests no configuration chang$
+if [ "$EXTRACT_RESULT" = "No Label has been assigned." ]
+then
+        echo "----------"
+        echo "No Infrastructure Configuration will be Performed ....."
+        echo "----------"
+        SERVER=false
+else
+        # Otherwise prints the Extraction result to the user.
+        echo "----------"
+        echo "SRS Configuration Analysis Complete. This application requires:"
+        echo $EXTRACT_RESULT "[<Entity> : <Confidence>]"
+        echo "----------"
+fi
+
+PLAYBOOK_NAME=`echo $EXTRACT_RESULT | cut -d' ' -f1`
 KEY_FILEPATH="/tmp/connectfile.pem"
-if [ $CLOUD_PROVIDER = "AWS" ]
+# Executes any required Ansible Playbooks to configure the new infrastructure.
+if [ $CLOUD_PROVIDER = "AWS" ] && [ $SERVER = true ]
 then
         # Obtains the nessecary output data from Terraform.
         SERVICE_NAME=$PROJECT_NAME"InfraService"
+        # Ensures script has access to Terraform outputs.
+        cd $INFRA_PATH/InfrastructureLib/Terraform/Projects/aws_$TEMPLATE_NAME
         # Writes the required key to a tmp file.
         terraform output ec2_private_key > $KEY_FILEPATH
         # Ensures KeyFile has the correct permissions level.
@@ -144,7 +189,28 @@ then
         echo "----------"
         echo "Beginning Ansible Playbook Configuration ....."
         echo "----------"
-        ansible-playbook -i ec2.py -l tag_Service_$SERVICE_NAME Playbooks/webserver_php.yml --key-file $KEY_FILEPATH --flush-cache
+        # Read the Extraction Model output into a Bash array.
+        IFS=' ' read -a EXTRACT_ARRAY <<< "${EXTRACT_RESULT}"
+        for playbook in "${!EXTRACT_ARRAY[@]}"
+        do
+                # Playbooks will always have even indexes.
+                if [ $((playbook%2)) -eq 0 ]
+                then
+                        PLAYBOOK_NAME=${EXTRACT_ARRAY[$playbook]}
+                        echo $PLAYBOOK_NAME
+                        ansible-playbook -i ec2.py -l tag_Service_$SERVICE_NAME Playbooks/$PLAYBOOK_NAME.yml --key-file $KEY_FILEPATH --flush-cache
+                fi
+        done
+elif [ $CLOUD_PROVIDER = "GOOGLE" ] && [ $SERVER = true ]
+then
+        echo "Google Clouud Support will come in the future."
+elif [ $CLOUD_PROVIDER = "AZURE" ] && [ $SERVER = true ]
+then
+        echo "Azure Support will come in the future."
+else
+        echo "----------"
+        echo "No Infrastructure Configuration is Required ....."
+        echo "----------"
 fi
 
 # Records the success of the Ansible operation.
@@ -162,8 +228,11 @@ if [ $TERRAFORM_SUCCESS -eq 0 ]
 then
         # Removes Project State information if required.
         terraform state list | xargs -L 1 terraform state rm >/dev/null 2> /dev/null
+        # Prevents Outputs from Conflicting.
+        sleep 2
         echo "----------"
-        echo "SRS Infrastructure has been successfully created for:"
+        echo "SRS Infrastructure has been successfully created and configured for:"
         echo $PROJECT_NAME
         echo "----------"
 fi
+
